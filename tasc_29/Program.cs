a1d57b60f3ccdd728ac8f888bfcce56b222fed1e
kim7887 <povelitel641@gmail.com>
2023-06-27 21:30:57 +0500

    Добавил 29 задачу

---
+++ tasc_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт
+// массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void ArraySet(int[] arr)
+{
+    for (int i = 1; i <= arr.Length; ++i)
+    {
+        Console.WriteLine($"задайте массиф {i}-элемент: ");
+        arr[i-1] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void ArrayPrint(int[] arr)
+{
+    for (int i = 1; i <= arr.Length; ++i)
+        Console.WriteLine(arr[i-1]);
+}
+int[] arr = new int[8];
+ArraySet(arr);
+ArrayPrint(arr);
